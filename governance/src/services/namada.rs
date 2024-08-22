20f6e3926537da132deb9ef781e1fc5a45e261bc
Joel Nordell <joel@unit410.com>
2024-08-22 17:06:35 -0500

    Update to SDK 0.43.0 and punt on governance for now since it no longer builds.

--- governance/src/services/namada.rs
+++ governance/src/services/namada.rs
@@ -2,7 +2,10 @@ use anyhow::Context;
 use futures::StreamExt;
 use namada_sdk::rpc;
 use shared::block::Epoch;
-use shared::proposal::{GovernanceProposalResult, GovernanceProposalStatus};
+use shared::proposal::{
+    // GovernanceProposalResult,
+    GovernanceProposalStatus,
+};
 use shared::utils::GovernanceProposalShort;
 use tendermint_rpc::HttpClient;
 
@@ -14,53 +17,54 @@ pub async fn query_last_epoch(client: &HttpClient) -> anyhow::Result<Epoch> {
 }
 
 pub async fn get_governance_proposals_updates(
-    client: &HttpClient,
+    _client: &HttpClient,
     proposal_data: Vec<GovernanceProposalShort>,
-    current_epoch: Epoch,
+    _current_epoch: Epoch,
 ) -> anyhow::Result<Vec<GovernanceProposalStatus>> {
-    let current_epoch = current_epoch as u64;
+    // let current_epoch = current_epoch as u64;
 
     Ok(futures::stream::iter(proposal_data)
         .filter_map(|proposal| async move {
             tracing::info!("Fetching proposal {} ...", proposal.id);
-            let proposal_result =
-                rpc::query_proposal_result(client, proposal.id).await;
+            // let proposal_result =
+            //     rpc::query_proposal_result(client, proposal.id).await;
             tracing::info!("Done fetching proposal {}!", proposal.id);
 
-            if let Ok(Some(proposal_result)) = proposal_result {
-                let result = if current_epoch.ge(&proposal.voting_end_epoch) {
-                    match proposal_result.result {
-                        namada_governance::utils::TallyResult::Passed => {
-                            GovernanceProposalResult::Passed
-                        }
-                        namada_governance::utils::TallyResult::Rejected => {
-                            GovernanceProposalResult::Rejected
-                        }
-                    }
-                } else if current_epoch.ge(&proposal.voting_start_epoch)
-                    && current_epoch.le(&proposal.voting_end_epoch)
-                {
-                    GovernanceProposalResult::VotingPeriod
-                } else {
-                    GovernanceProposalResult::Pending
-                };
-
-                Some(GovernanceProposalStatus {
-                    id: proposal.id,
-                    result,
-                    yay_votes: proposal_result
-                        .total_yay_power
-                        .to_string_native(),
-                    nay_votes: proposal_result
-                        .total_nay_power
-                        .to_string_native(),
-                    abstain_votes: proposal_result
-                        .total_abstain_power
-                        .to_string_native(),
-                })
-            } else {
-                None
-            }
+            None
+            // if let Ok(Some(proposal_result)) = proposal_result {
+            //     let result = if current_epoch.ge(&proposal.voting_end_epoch) {
+            //         match proposal_result.result {
+            //             namada_governance::utils::TallyResult::Passed => {
+            //                 GovernanceProposalResult::Passed
+            //             }
+            //             namada_governance::utils::TallyResult::Rejected => {
+            //                 GovernanceProposalResult::Rejected
+            //             }
+            //         }
+            //     } else if current_epoch.ge(&proposal.voting_start_epoch)
+            //         && current_epoch.le(&proposal.voting_end_epoch)
+            //     {
+            //         GovernanceProposalResult::VotingPeriod
+            //     } else {
+            //         GovernanceProposalResult::Pending
+            //     };
+            //
+            //     Some(GovernanceProposalStatus {
+            //         id: proposal.id,
+            //         result,
+            //         yay_votes: proposal_result
+            //             .total_yay_power
+            //             .to_string_native(),
+            //         nay_votes: proposal_result
+            //             .total_nay_power
+            //             .to_string_native(),
+            //         abstain_votes: proposal_result
+            //             .total_abstain_power
+            //             .to_string_native(),
+            //     })
+            // } else {
+            //     None
+            // }
         })
         .map(futures::future::ready)
         .buffer_unordered(20)
